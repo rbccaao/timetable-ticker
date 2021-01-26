429ddf32f1e8bf18995bedd39819cf731a055389
rbccaao <42102397+rbccaao@users.noreply.github.com>
2021-01-26 14:58:33 +0900

    Initial commit
    - Webpage with time telling and showing functionality
    - Don't look at the script file, it's disgusting.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# timetable-ticker
- What period is it? 今は何時限ですか？
+# Timetable Ticker - What period is it? 今は何時限ですか？
+
+A simple web page to tell the current time and school period at A Certain School. 
+
+Made using HTML, Javascript, and Bootstrap 5. 
+
+## Background
+
+This was created in a response to the ever changing schedule at the school I work at. The school timetable has shifted around a few times since the COVID-19 pandemic. So instead of constantly wondering what period we're in, I'll let the computer do the maths.
+
+To be honest, I'm just really lazy and I don't like doing calculations multiple times a day. ¯\\_(ツ)_/¯
---
+++ script.js
@@ -0,0 +1,306 @@
+var setPeriodLength = 0;
+var setBreakLength = 0;
+var homeroomLength = 20;
+var lunchLength = 45;
+var postLunchLength = 5;
+var cleaningLength = 15;
+
+var weekdayItems = ["Homeroom - 朝礼","Period 1 - １時限","Break","Period 2 - ２時限","Break","Period 3 - ３時限","Break","Period 4 - ４時限","Lunch - 昼休み","Break - 予鈴","Period 5 - ５時限","Break","Period 6 - ６時限","Afternoon Homeroom - 終礼","Cleaning - 掃除","End of school"]
+var weekendItems = ["Homeroom - 朝礼","Period 1 - １時限","Break","Period 2 - ２時限","Break","Period 3 - ３時限","Break","Period 4 - ４時限","Afternoon Homeroom - 終礼","Cleaning - 掃除","End of school"]
+
+var schedule = [];
+var morningHomeroom = moment({hour: 8, minute: 25});
+
+var currentItem;
+var weekdayOrEnd;
+
+moment.relativeTimeThreshold('m', 60*24*30*12);
+
+document.addEventListener('DOMContentLoaded', function () {
+	autoSetForm();
+	console.log(document.cookie);
+
+	printCurrentTime();
+	calculateIt();
+	console.log("ready!");
+});
+
+setInterval(function() {
+	printCurrentTime();
+	if (moment().isBetween(schedule[0]["startTime"], schedule[schedule.length-1]["endTime"])) {
+		if (moment().isSameOrAfter(currentItem["endTime"])) {
+			calculateIt();
+		}
+		printItemTimes();
+	}
+}, 1000); 
+
+function autoSetForm() {
+	if (moment().days() > 0 && moment().days() < 6) {
+		weekdayOrEnd = 'weekday';
+	} else {
+		weekdayOrEnd = 'weekend';
+	}
+
+	if (document.cookie) {
+		setPeriodLength = getCookie("setPeriodLength");
+		setBreakLength = getCookie("setBreakLength");
+
+	    var periodLength = document.getElementById(periodLength);
+	    periodLength.value = setPeriodLength;
+	    var breakLength = document.getElementById(breakLength);
+	    breakLength.value = setBreakLength;
+	}
+}
+
+function printCurrentTime() {
+	var currentDay = document.getElementById("currentDay");
+	currentDay.innerHTML = moment().format('dddd');
+
+	var currentDayJp = document.getElementById("currentDayJp");
+	currentDayJp.innerHTML = getJpDay();
+
+	var currentTime = document.getElementById("currentTime");
+	currentTime.innerHTML = moment().format('HH:mm:ss');
+}
+
+function getJpDay() {
+	var jp;
+	switch (moment().days()) {
+		case 0:
+		jp = "日";
+		break;
+		case 1:
+		jp = "月";
+		break;
+		case 2:
+		jp = "火";
+		break;
+		case 3:
+		jp = "水";
+		break;
+		case 4:
+		jp = "木";
+		break;
+		case 5:
+		jp = "金";
+		break;
+		case 6:
+		jp = "土";
+		break;
+	}
+	return "(" + jp + ")";
+}
+
+function printItemTimes() {
+	var elapsedTime = document.getElementById("elapsedTime");
+	elapsedTime.innerHTML = currentItem["startTime"].fromNow(true);
+
+	var remainingTime = document.getElementById("remainingTime");
+	remainingTime.innerHTML = currentItem["endTime"].fromNow(true);
+
+	var milliCurrent = moment().unix();
+	var milliStart = currentItem["startTime"].unix();
+	var milliEnd = currentItem["endTime"].unix();
+	var percent = ((milliCurrent - milliStart) / (milliEnd - milliStart)) * 100;
+
+	var progressBar = document.getElementById("progressBar");
+	progressBar.style.width = percent+"%";
+}
+
+function calculateIt() {
+	var selectPeriodLength = document.getElementById("periodLength");
+	setPeriodLength = selectPeriodLength.options[selectPeriodLength.selectedIndex].value;
+
+	var selectBreakLength = document.getElementById("breakLength");
+	setBreakLength = selectBreakLength.options[selectBreakLength.selectedIndex].value;
+	setCookies();
+	createSchedule();
+	printSchedule();
+}
+
+function setCookies() {
+	var expiryDate = new Date();
+	expiryDate.setMonth(expiryDate.getMonth() + 1);
+
+	document.cookie = "setPeriodLength=" + setPeriodLength + "; expires=" + expiryDate;
+	document.cookie = "setBreakLength=" + setBreakLength + "; expires=" + expiryDate;
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function createSchedule() {
+	if (weekdayOrEnd === "weekday") {
+		var dayItems = weekdayItems;
+	} else {
+		var dayItems = weekendItems;
+	}
+
+	schedule.length = 0;
+
+	schedule.push({
+		"name": dayItems[0],
+		"startTime": morningHomeroom,
+		"endTime": morningHomeroom.clone().add(homeroomLength, 'm'),
+	})
+
+	for (var i = 1; i < dayItems.length; i++) {
+		previousItem = schedule[i-1];
+
+		if (dayItems[i].includes("Period")) {
+			schedule[i] = {
+				"name": dayItems[i],
+				"startTime": previousItem["endTime"],
+				"endTime": previousItem["endTime"].clone().add(setPeriodLength, 'm')
+			}
+		} else if (dayItems[i].includes("Break")) {
+			schedule[i] = {
+				"name": dayItems[i],
+				"startTime": previousItem["endTime"],
+				"endTime": previousItem["endTime"].clone().add(setBreakLength, 'm')
+			}
+		} else if (dayItems[i].includes("Homeroom")) {
+			schedule[i] = {
+				"name": dayItems[i],
+				"startTime": previousItem["endTime"],
+				"endTime": previousItem["endTime"].clone().add(homeroomLength, 'm')
+			}
+		} else if(dayItems[i].includes("Lunch")) {
+			schedule[i] = {
+				"name": dayItems[i],
+				"startTime": previousItem["endTime"],
+				"endTime": previousItem["endTime"].clone().add(lunchLength, 'm')
+			}
+
+			schedule[i+1] = {
+				"name": dayItems[i+1],
+				"startTime": schedule[i]["endTime"],
+				"endTime": schedule[i]["endTime"].clone().add(postLunchLength, 'm')
+			}
+			i++;
+		} else if (dayItems[i].includes("Cleaning")) {
+			schedule[i] = {
+				"name": dayItems[i],
+				"startTime": previousItem["endTime"],
+				"endTime": previousItem["endTime"].clone().add(cleaningLength, 'm')
+			}
+		} else if (dayItems[i].includes("End")) {
+			schedule[i] = {
+				"name": dayItems[i],
+				"startTime": previousItem["endTime"],
+				"endTime": previousItem["endTime"].clone().add(10, 'm')
+			}
+		}
+	}
+}
+
+function printSchedule() {
+	var output = document.getElementById("output");
+	output.innerHTML = "";
+
+	var totalResult = "";
+	var currentSlot = false;
+	var pastSlot = true;
+	var now = moment();
+
+	//time debugger
+	// var now = moment({hour: 8, minute: 45});
+
+	if (now.isBefore(morningHomeroom)) {
+		pastSlot = false;
+	}
+
+	for (var i = 0; i < schedule.length-1; i++) {
+
+		var item = schedule[i];
+		var result = '';
+
+		currentSlot = (now.isBetween(item["startTime"], item["endTime"], undefined, '[)'));
+
+		if (!item["name"].includes("Break")) { //period
+			if (currentSlot) {
+				result += '<div class="card mb-1 bg-success bg-gradient text-white current-slot">';
+				pastSlot = false;
+			} else if (pastSlot) {
+				result += '<div class="card mb-1 bg-secondary text-white-50 past-slot">';
+			} else {
+				result += '<div class="card mb-1 bg-white future-slot">';
+			}
+
+			result += '<div class="row g-0 "><div class="col-6"><div class="card-body">';
+			result += '<h2 class="m-0 fs-6">'+ item["name"] + '</h2>';
+			result += '</div></div><div class="col-6"><div class="card-body"><p class="card-text">';
+			result += item["startTime"].format('HH:mm') + " ~ " + item["endTime"].format('HH:mm') + '</p>';
+			result += '</div></div></div>';
+		} else { //break
+			if (currentSlot) {
+				result += '<div class="card mb-1">';
+				result += '<div class="row g-0">';
+				result += '<div class="col"><div class="card-body bg-info text-white">';
+				result += '<p class="m-0 fs-6">'+ item["name"] + '</p>';
+				result += '</div></div></div>';
+				pastSlot = false;
+			}
+		}
+
+		if (currentSlot) {
+			currentItem = schedule[i];
+			if (!item["name"].includes("Break")) {
+				result += '<div class="row g-0 card-footer bg-light text-success">';
+			} else {
+				result += '<div class="row g-0 card-footer bg-light text-info">';
+			}
+			result += '<div class="col-6">';
+			result += '<p class="m-0">Since start: <span id="elapsedTime">00:00:00</span></p>'
+			result += '</div><div class="col-6">'
+			result += '<p class="m-0">Till end: <span id="remainingTime">00:00:00</span></p>'
+			result += '</div></div>';
+			result += '<div class="row g-0">';
+			result += '<div class="progress" style="height: 5px;">';
+			if (!item["name"].includes("Break")) {
+				result += '<div id="progressBar" class="progress-bar bg-success" style="width: 0%" role="progressbar"></div>';
+			} else {
+				result += '<div id="progressBar" class="progress-bar bg-info" style="width: 0%" role="progressbar"></div>';
+			}
+			result += '</div></div>';
+		}
+
+		result += '</div>';
+		totalResult += result;
+		currentSlot = false;
+	}
+
+	var lastItem = schedule[schedule.length-1];
+	if (pastSlot) {
+		result = '<div class="card mb-1 bg-warning">';
+		currentItem = null;
+	} else {
+		result = '<div class="card mb-1 bg-white">';
+	}
+	result += '<div class="row g-0"><div class="col"><div class="card-body">';
+	result += '<h2 class="m-0 fs-6">'+ lastItem["name"] + '</h2>';
+	result += '</div></div></div>';
+	totalResult += result;
+
+	output.innerHTML = totalResult;
+}
+
+function testing() {
+	var now = moment();
+	var result = now.isBetween(moment({hour: 8, minute: 0}), moment({hour: 9, minute: 0}));
+	console.log(result);
+}
